4377cec302c76f2f63f1b70d766af0ae1b9dbd18
Gabrieira <gabrielhgco@gmail.com>
2023-04-04 14:21:12 -0300

    Adicionando o CRUD de usuário.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddScoped<ExoContext, ExoContext>();
-builder.Services.AddControllers();
-builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
-
-var app = builder.Build();
-
-app.UseRouting();
-
-app.UseEndpoints(endpoints =>
+internal class Program
 {
-    endpoints.MapControllers();
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services.AddScoped<ExoContext, ExoContext>();
+        builder.Services.AddControllers();
+        builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+        builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
+
+        var app = builder.Build();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.Run();
+    }
+}
