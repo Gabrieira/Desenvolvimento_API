557650cbe1a0801b6fff541805645991622248a0
Gabrieira <gabrielhgco@gmail.com>
2023-04-04 18:15:51 -0300

    Adição de método login e CORS.

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,6 +3,10 @@ using Exo.WebApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
 namespace Exo.WebApi.Controllers
 {
     [Produces("application/json")]
@@ -23,13 +27,52 @@ namespace Exo.WebApi.Controllers
             return Ok(_usuarioRepository.Listar());
         }
         // post -> /api/usuarios
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
-        {
-            _usuarioRepository.Cadastrar(usuario);
-            return StatusCode(201);
-        }
+        // [HttpPost]
+        // public IActionResult Cadastrar(Usuario usuario)
+        // {
+        //     _usuarioRepository.Cadastrar(usuario);
+        //     return StatusCode(201);
+        // }
         // get -> /api/usuarios/{id}
+
+        // Novo código POST para auxiliar o método de Login.
+        public IActionResult Post(Usuario usuario)
+        {
+            Usuario usuarioBuscado = _usuarioRepository.Login(usuario.Email,
+            usuario.Senha);
+            if (usuarioBuscado == null)
+            {
+                return NotFound("E-mail ou senha inválidos!");
+            }
+            // Se o usuário for encontrado, segue a criação do token.
+            // Define os dados que serão fornecidos no token - Payload.
+            var claims = new[]
+            {
+// Armazena na claim o e-mail usuário autenticado.
+                new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+// Armazena na claim o id do usuário autenticado.
+                new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.Id.ToString()),
+};
+            // Define a chave de acesso ao token.
+            var key = new
+            SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapi-chaveautenticacao"));
+            // Define as credenciais do token.
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // Gera o token.
+            var token = new JwtSecurityToken(
+            issuer: "exoapi.webapi", // Emissor do token.
+            audience: "exoapi.webapi", // Destinatário do token.
+            claims: claims, // Dados definidos acima.
+            expires: DateTime.Now.AddMinutes(30), // Tempo de expiração.
+            signingCredentials: creds // Credenciais do token.
+            );
+            // Retorna ok com o token.
+            return Ok(
+            new { token = new JwtSecurityTokenHandler().WriteToken(token) }
+            );
+        }
+        // Fim do novo código POST para auxiliar o método de Login.
+
         [HttpGet("{id}")] // Faz a busca pelo ID.
         public IActionResult BuscarPorId(int id)
         {
@@ -42,13 +85,16 @@ namespace Exo.WebApi.Controllers
         }
         // put -> /api/usuarios/{id}
         // Atualiza.
+        [Authorize]
         [HttpPut("{id}")]
+
         public IActionResult Atualizar(int id, Usuario usuario)
         {
             _usuarioRepository.Atualizar(id, usuario);
             return StatusCode(204);
         }
         // delete -> /api/usuarios/{id}
+        [Authorize]
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
--- Program.cs
+++ Program.cs
@@ -1,26 +1,66 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
+
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         builder.Services.AddScoped<ExoContext, ExoContext>();
         builder.Services.AddControllers();
-        builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
-        builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
-
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        // Forma de autenticacão.
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = "JwtBearer";
+            options.DefaultChallengeScheme = "JwtBearer";
+        })
+        // Parâmetros de validacão do token.
+        .AddJwtBearer("JwtBearer", options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                // Valida quem está solicitando.
+                ValidateIssuer = true,
+                // Valida quem está recebendo.
+                ValidateAudience = true,
+                // Define se o tempo de expiração será validado.
+                ValidateLifetime = true,
+                // Criptografia e validação da chave de autenticacão.
+                IssuerSigningKey = new
+    SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exoapichave-autenticacao")),
+                // Valida o tempo de expiração do token.
+                ClockSkew = TimeSpan.FromMinutes(30),
+                // Nome do issuer, da origem.
+                ValidIssuer = "exoapi.webapi",
+                // Nome do audience, para o destino.
+                ValidAudience = "exoapi.webapi"
+            };
+        });
+        builder.Services.AddTransient<ProjetoRepository,
+        ProjetoRepository>();
+        builder.Services.AddTransient<UsuarioRepository,
+        UsuarioRepository>();
         var app = builder.Build();
-
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
         app.UseRouting();
-
+        // Habilita a autenticação.
+        app.UseAuthentication();
+        // Habilita a autorização.
+        app.UseAuthorization();
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
         });
-
         app.Run();
+
     }
 }
